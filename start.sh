0b94286f70109b8f39fc60aadc2b7362ccbcce67
spencergoff <spencer.goff95@yahoo.com>
2022-02-04 13:20:57 -0800

    getting sam deploy to work

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -ex
 function main {
     file_hash="QmdLMnwpbi8xPFcpRGqsfHCRGVgovV3yAv8fUtoug66Wqj"
     check_internet_connection
-    wget https://ipfs.io/ipfs/$file_hash
+    curl https://ipfs.io/ipfs/$file_hash
     setup_ipfs
     #add_pinata_peers
     echo "~/.ipfs/config: "
