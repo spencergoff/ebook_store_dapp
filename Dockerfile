634d0889e7820f425b70f9703cf4c7f2a149ba67
spencergoff <spencer.goff95@yahoo.com>
2022-02-05 11:08:28 -0800

    migrated to Python

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,4 @@
-FROM alpine:3.9.3
-COPY start.sh /start.sh
-RUN apk update \
-    && apk add jq \
-    && apk add python3 \
-    && apk add py-pip \
-    && apk add gnupg \
-    && wget https://dist.ipfs.io/go-ipfs/v0.11.0/go-ipfs_v0.11.0_linux-amd64.tar.gz \
-    && tar -xvzf go-ipfs_v0.11.0_linux-amd64.tar.gz \
-    && cd go-ipfs \
-    && apk add --no-cache bash \    
-    && bash install.sh \
-    && wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub \
-    && wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.34-r0/glibc-2.34-r0.apk \
-    && apk add glibc-2.34-r0.apk \
-    && chmod 777 /start.sh \
-    && apk add curl
-RUN python3 -m pip install -r requirements.txt -t .
-CMD [ "python", "download_decrypt.py" ]
+FROM public.ecr.aws/lambda/python:3.9
+COPY download_decrypt.py requirements.txt decrypted_file.txt ./
+RUN python3.9 -m pip install -r requirements.txt -t .
+CMD [ "download_decrypt.main" ]
---
+++ download_decrypt.py
@@ -0,0 +1,57 @@
+import json
+import boto3
+import gnupg
+import pickle
+import requests
+
+gpg = gnupg.GPG()
+file_hash = 'QmdLMnwpbi8xPFcpRGqsfHCRGVgovV3yAv8fUtoug66Wqj'
+encrypted_file_path = 'encrypted_file.txt'
+decrypted_file_path = 'decrypted_file.txt'
+gpg.encoding = 'utf-8'
+
+def main():
+    download_file()
+    decrypt_the_file()
+
+def download_file():
+    file_response = requests.get(f'https://ipfs.io/ipfs/{file_hash}')
+    print(f'file_response.text: {file_response.text}')
+    with open(encrypted_file_path, 'w') as f:
+        f.write(file_response.text)
+
+def decrypt_the_file():
+    print('Made it to decrypt_file!')
+    secret = get_secret('ebook_decryption_secret')
+    print(f'len(secret): {len(secret)}')
+    with open(encrypted_file_path, 'r+') as f:
+        encrypted_string = str(f.read())
+    print(f'encrypted_string: {encrypted_string}')
+    decryption = gpg.decrypt(message=encrypted_string, passphrase=secret)
+    print(f'decryption.status: {decryption.status} | decryption.ok: {decryption.ok} | decryption.data: {decryption.data}')
+    with open(decrypted_file_path, 'r') as f:
+        print(f'decrypted_file: {f.read()}')
+
+def get_secret(secret_name):
+    print(f'Made it to get_secret with secret_name: {secret_name}')
+    region_name = 'us-west-2'
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+    secret_payload = client.get_secret_value(SecretId=secret_name)
+    secret_value = extract_secret_from_payload(secret_name, secret_payload)
+    return secret_value
+
+def extract_secret_from_payload(secret_name, secret_payload):
+    try:
+        all_secrets = json.loads(secret_payload['SecretString'])
+    except:
+        secret_value = secret_payload['SecretString']
+    else:
+        secret_value = all_secrets[secret_name]
+    return secret_value
+
+if __name__ == '__main__':
+    main()
