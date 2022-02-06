d5ea3579adc279794431a69e6c804ffd22a871d9
spencergoff <spencer.goff95@yahoo.com>
2022-02-06 11:26:44 -0800

    dev

--- start.sh
+++ start.sh
@@ -1,48 +1,6 @@
-#!/bin/bash
-set -ex
-
 function main {
-    file_hash="QmdLMnwpbi8xPFcpRGqsfHCRGVgovV3yAv8fUtoug66Wqj"
-    check_internet_connection
-    curl https://ipfs.io/ipfs/$file_hash
-    # setup_ipfs
-    # #add_pinata_peers
-    # echo "~/.ipfs/config: "
-    # cat ~/.ipfs/config
-    # download_file
-    decrypt_file
-    cat "$file_hash."
-}
-
-function check_internet_connection {
-    connectivity=$(ping -q -c1 8.8.8.8 &>/dev/null && echo online || echo offline)
-    echo "connectivity: $connectivity"
-    # if [[ $connectivity == "offline" ]]; then
-    #     exit 1
-    # fi
-}
-
-function setup_ipfs {
-    ipfs init
-    ipfs daemon &
-    sleep 2
-}
-
-function add_pinata_peers {
-    original_ipfs_confg=$(cat ~/.ipfs/config)
-    pinata_peers=$(cat pinata_peers.json)
-    jq '.Peering.Peers = "$pinata_peers"' <<< "$original_ipfs_confg"
-    echo "$original_ipfs_confg" > ~/.ipfs/config
-}
-
-function download_file {
-    ipfs get $file_hash
-}
-
-function decrypt_file {
-    echo "Trying to decrypt $file_hash..."
-    secret=$(python -c import get_secret; get_secret)
-    gpg --yes --batch --passphrase=[Enter your passphrase here] -c filename.txt
+    python3 download_decrypt.py
+    gpg --yes --batch --passphrase=$secret_value test.txt.gpg
 }
 
 main
