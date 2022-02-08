55c3bc62d01cfae7ccf45dce638b46f2c298d5b5
Spencer Goff <spencer.goff95@yahoo.com>
2022-02-07 16:05:10 -0800

    Create README (#1)
    Lambda function can download a file from IPFS and decrypt it

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,4 @@
-FROM alpine:3.9.3
-COPY --chmod=700 test.txt.gpg /test.txt.gpg
-COPY --chmod=700 start.sh /start.sh
-COPY --chmod=700 pinata_peers.json /pinata_peers.json
-RUN apk update \
-    && apk add jq \
-    && apk add gnupg \
-    && wget https://dist.ipfs.io/go-ipfs/v0.11.0/go-ipfs_v0.11.0_linux-amd64.tar.gz \
-    && tar -xvzf go-ipfs_v0.11.0_linux-amd64.tar.gz \
-    && cd go-ipfs \
-    && apk add --no-cache bash \    
-    && bash install.sh \
-    && wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub \
-    && wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.34-r0/glibc-2.34-r0.apk \
-    && apk add glibc-2.34-r0.apk
-ENTRYPOINT [ "./start.sh" ]
+FROM public.ecr.aws/lambda/python:3.9
+COPY download_decrypt.py requirements.txt ./
+RUN python3.9 -m pip install -r requirements.txt -t .
+CMD [ "download_decrypt.main" ]
---
+++ download_decrypt.py
@@ -0,0 +1,52 @@
+import json
+import boto3
+import requests
+from cryptography.fernet import Fernet
+
+def main(event, context):
+    content_id = 'QmXjvurAQ3MLpxGQM6NvdgPC8uK1YndmEBmRzCEJJUgEz2'
+    key = get_key('ebook_decryption_secret')
+    decrypt_content_with_key(content_id, key)
+
+def decrypt_content_with_key(content_id, key):
+    f = Fernet(key)
+    url = f'https://ipfs.io/ipfs/{content_id}'
+    print(f'Trying to get encrypted message from: {url}')
+    encrypted_message = requests.get(url).text
+    encrypted_message_as_bytes = encrypted_message.encode('utf-8')
+    decrypted_message = f.decrypt(encrypted_message_as_bytes)
+    print(f'decrypted_message: {decrypted_message}')
+
+def get_key(secret_name):
+    print(f'Made it to get_secret with secret_name: {secret_name}')
+    region_name = 'us-west-2'
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+    secret_payload = client.get_secret_value(SecretId=secret_name)
+    secret_value = extract_secret_from_payload(secret_name, secret_payload)
+    return secret_value
+
+def extract_secret_from_payload(secret_name, secret_payload):
+    try:
+        all_secrets = json.loads(secret_payload['SecretString'])
+    except:
+        secret_value = secret_payload['SecretString']
+    else:
+        secret_value = all_secrets[secret_name]
+    return secret_value
+
+def encrypt_message(message):
+    print(f'message to encrypt: {message}')
+    encrypted_file_path = '/tmp/fernet_encrypted.txt'
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    encrypted_message = f.encrypt(message)
+    with open(encrypted_file_path, 'wb') as f:
+        f.write(encrypted_message)
+        print(f'Wrote the encrypted message to {encrypted_file_path}')
+
+if __name__ == '__main__':
+    main(None, None)
