93afef1f3fc55ba340dbfd3e70a4b9c000042ba9
Spencer Goff <spencer.goff95@yahoo.com>
2022-02-08 17:57:04 -0800

    Added smart contract to facilitate payments and permissions

---
+++ permissions.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract FilePermissions {
+
+    address payable public owner;
+    address[] public customers;
+    uint256 price_of_file = 1000;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function deposit() public payable {
+        if (msg.value >= price_of_file && !is_customer(msg.sender)) {
+            customers.push(msg.sender);
+        }
+    }
+
+    function is_customer(address sender_address) private view returns (bool) {
+        for (uint256 i = 0; i < customers.length; i++) {
+            if (customers[i] == sender_address) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function get_customers() public view returns (address[] memory) {
+        return customers;
+    }
+
+    function get_contract_balance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+}
