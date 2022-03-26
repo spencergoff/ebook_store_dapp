c181a41c178103ccdad4dfb3cdab211c4d1d0f53
spencergoff <spencer.goff95@yahoo.com>
2022-03-26 11:10:02 -0700

    emit event logs from test

--- test/TestFilePermissions.sol
+++ test/TestFilePermissions.sol
@@ -8,6 +8,7 @@ import "../contracts/FilePermissions.sol";
 contract TestFilePermissions {
 
     uint public initialBalance = 1 ether;
+    event LogAddressArray(string arrayName, address[] addressArray);
 
     function testNewSender_whenSendingSufficientFunds_shouldBeAddedToCustomers() public {
         FilePermissions filePermissions = FilePermissions(DeployedAddresses.FilePermissions());
@@ -15,6 +16,8 @@ contract TestFilePermissions {
         address[] memory actualValue = filePermissions.get_customers();
         address[] memory expectedValue = new address[](1);
         expectedValue[0] = address(this);
-        Assert.equal(actualValue, expectedValue, "This is a string");
+        emit LogAddressArray("actualValue", actualValue);
+        emit LogAddressArray("expectedValue", expectedValue);
+        Assert.equal(actualValue, expectedValue, "Error: New sender wasn't added to customers[]");
     }
 }
